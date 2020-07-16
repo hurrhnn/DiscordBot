8ea7707b0b7fc4b44ff6a0d76b98f11b785748f8
hurrhnn <40728528+hurrhnn@users.noreply.github.com>
2020-07-16 22:39:03 +0900

    Added GuildMusicInfo class

---
+++ src/main/java/xyz/hurrhnn/discordbot/cmd/music/GuildMusicInfo.java
@@ -0,0 +1,9 @@
+package xyz.hurrhnn.discordbot.cmd.music;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class GuildMusicInfo {
+    public static Map<String, Boolean> isGuildSkipRequestDelayed = new HashMap<>();
+    public static Map<String, Boolean> isGuildSetRepeatMusic = new HashMap<>();
+}
