401dc924497649dc4403195fe17d586d68fe7161
hurrhnn <40728528+hurrhnn@users.noreply.github.com>
2020-07-16 22:50:00 +0900

    Modularization - VolumeCommand

---
+++ src/main/java/xyz/hurrhnn/discordbot/cmd/music/VolumeCommand.java
@@ -0,0 +1,44 @@
+package xyz.hurrhnn.discordbot.cmd.music;
+
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
+import me.duncte123.botcommons.messaging.EmbedUtils;
+import net.dv8tion.jda.api.entities.TextChannel;
+import xyz.hurrhnn.discordbot.cmd.CmdContext;
+import xyz.hurrhnn.discordbot.cmd.ICmd;
+
+import java.util.Collections;
+import java.util.List;
+
+public class VolumeCommand implements ICmd {
+    @Override
+    public void handle(CmdContext cmdContext) {
+
+        TextChannel textChannel = cmdContext.getChannel();
+        AudioPlayer audioPlayer = PlayerManager.getInstance().getGuildMusicManager(cmdContext.getGuild()).player;
+
+        if(isArgsEmpty(cmdContext.getArgs()))
+            textChannel.sendMessage(EmbedUtils.embedMessageWithTitle("Music - Volume", "Current Volume: " + audioPlayer.getVolume()).build()).queue();
+
+        try {
+            audioPlayer.setVolume(Integer.parseInt(cmdContext.getArgs().get(0)));
+            textChannel.sendMessage(EmbedUtils.embedMessageWithTitle("Music - Volume", ("The volume for the music player is set to " + audioPlayer.getVolume() + ".")).build()).queue();
+        }catch (NumberFormatException ignored) {
+            textChannel.sendMessage(EmbedUtils.embedMessageWithTitle("Music - Volume", "```E: Volume must be integer!").build()).queue();
+        }
+    }
+
+    @Override
+    public String getName() {
+        return "volume";
+    }
+
+    @Override
+    public String getHelp() {
+        return null;
+    }
+
+    @Override
+    public List<String> getAliases() {
+        return Collections.singletonList("v");
+    }
+}
