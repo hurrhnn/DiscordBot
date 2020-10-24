88af9257ea7208a06ba4f2a4ef6b425a55385b97
hurrhnn <40728528+hurrhnn@users.noreply.github.com>
2020-10-24 13:06:30 +0900

    AudioSendHandler - clean code

--- src/main/java/xyz/hurrhnn/discordbot/cmd/music/AudioPlayerSendHandler.java
+++ src/main/java/xyz/hurrhnn/discordbot/cmd/music/AudioPlayerSendHandler.java
@@ -1,6 +1,5 @@
 package xyz.hurrhnn.discordbot.cmd.music;
 
-
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
 import com.sedmelluq.discord.lavaplayer.track.playback.AudioFrame;
 import net.dv8tion.jda.api.audio.AudioSendHandler;
