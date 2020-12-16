e240a65b769e33ac2f9b14175aaef58de3984893
hurrhnn <40728528+hurrhnn@users.noreply.github.com>
2020-12-17 00:01:25 +0900

    CommandManager - Add MP3Command function.

--- src/main/java/xyz/hurrhnn/discordbot/cmd/CommandManager.java
+++ src/main/java/xyz/hurrhnn/discordbot/cmd/CommandManager.java
@@ -5,6 +5,7 @@ import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import xyz.hurrhnn.discordbot.cmd.server.ServerCommand;
 import xyz.hurrhnn.discordbot.util.Info;
+
 import javax.annotation.Nullable;
 import java.awt.*;
 import java.util.ArrayList;
@@ -12,10 +13,10 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Pattern;
 
-public class CommandManager extends Thread{
+public class CommandManager extends Thread {
     private final List<ICmd> commands = new ArrayList<>();
 
-    public CommandManager(){
+    public CommandManager() {
         addCommand(new CleanChatCommand());
         addCommand(new DebugCommand());
         addCommand(new MemeCommand());
@@ -26,12 +27,13 @@ public class CommandManager extends Thread{
         addCommand(new PrefixCommand());
         addCommand(new ServerCommand());
         addCommand(new InfoCommand());
+        addCommand(new MP3Command());
     }
 
     private void addCommand(ICmd cmd) {
         boolean nameFound = this.commands.stream().anyMatch((it) -> it.getName().equalsIgnoreCase(cmd.getName()));
 
-        if(nameFound) {
+        if (nameFound) {
             throw new IllegalArgumentException("A command with this name is already present.");
         }
 
@@ -46,15 +48,13 @@ public class CommandManager extends Thread{
     public ICmd getCommand(String search) {
         String searchLower = search.toLowerCase();
 
-        for(ICmd cmd : this.commands) {
-            if(cmd.getName().equals(searchLower) || cmd.getAliases().contains(searchLower)) return cmd;
+        for (ICmd cmd : this.commands) {
+            if (cmd.getName().equals(searchLower) || cmd.getAliases().contains(searchLower)) return cmd;
         }
-
         return null;
     }
 
-    public void handle(GuildMessageReceivedEvent event)
-    {
+    public void handle(GuildMessageReceivedEvent event) {
 
         EmbedUtils.setEmbedBuilder(
                 () -> new EmbedBuilder()
@@ -62,7 +62,8 @@ public class CommandManager extends Thread{
                         .setFooter("Requested By " + event.getAuthor().getAsTag(), event.getAuthor().getAvatarUrl())
         );
 
-        if(event.getMessage().getContentRaw().equals("?prefix")) event.getChannel().sendMessage(EmbedUtils.embedMessageWithTitle("Prefix", "The prefix " + event.getJDA().getSelfUser().getName() + " bot uses on this server: `" + Info.getPrefix(event)+"`").build()).queue();
+        if (event.getMessage().getContentRaw().equals("?prefix"))
+            event.getChannel().sendMessage(EmbedUtils.embedMessageWithTitle("Prefix", "The prefix " + event.getJDA().getSelfUser().getName() + " bot uses on this server: `" + Info.getPrefix(event) + "`").build()).queue();
 
         String[] split = event.getMessage().getContentRaw()
                 .replaceFirst("(?i)" + Pattern.quote(Info.getPrefix(event)), "")
@@ -71,8 +72,7 @@ public class CommandManager extends Thread{
         String invoke = split[0].toLowerCase();
         ICmd cmd = this.getCommand(invoke);
 
-        if(cmd != null)
-        {
+        if (cmd != null) {
             List<String> args = Arrays.asList(split).subList(1, split.length);
 
             CmdContext cmdContext = new CmdContext(event, args);
